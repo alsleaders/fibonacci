7ab546321c4eef9efc15aa678b89a0a3fd346ee2
Amanda Leaders <alsleaders@gmail.com>
2019-06-04 12:26:13 -0400

    counts on timer, add request field next

--- src/components/FibWithClass.js
+++ src/components/FibWithClass.js
@@ -6,19 +6,35 @@ export default function FibWithClass() {
   const [current, setCurrent] = useState(1)
   const [timer, setTimer] = useState(1)
 
+  const [seq, setSeq] = useState([1, 1])
+
   console.log('did this button work')
   console.log({ twoAgo })
   console.log({ lastOne })
   console.log({ current })
 
+  const addFibArray = () => {
+    setSeq(prevSeq => {
+      const two = prevSeq[prevSeq.length - 2]
+      const one = prevSeq[prevSeq.length - 1]
+      const next = two + one
+      const rv = prevSeq.concat(next)
+      console.log({ two, one, next, rv })
+      setCurrent(next)
+      return rv
+    })
+  }
+
   const addFib = () => {
-    setCurrent(current + lastOne)
+    console.log(current, lastOne)
+    setCurrent(prevCurrent => prevCurrent + lastOne)
     setLastOne(current)
     setTwoAgo(lastOne)
   }
 
   useEffect(() => {
     setInterval(() => {
+      addFibArray()
       setTimer(lastSecond => lastSecond + 1)
     }, 1000)
   }, '')
@@ -27,7 +43,7 @@ export default function FibWithClass() {
     <>
       <h1>Fibonacci Numbers!</h1>
       <main className="be-centered">
-        <button onClick={addFib}>Next number</button>
+        <button onClick={addFibArray}>Next number</button>
         <p className="display">{current}</p>
         <button
           onClick={() => {
@@ -38,7 +54,7 @@ export default function FibWithClass() {
         >
           Reset
         </button>
-        {/* <h3>{timer}</h3> */}
+        <h3>{timer}</h3>
       </main>
     </>
   )
