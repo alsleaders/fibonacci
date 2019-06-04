97e54670bfe53e4ebb13b02fef580295222cb79d
Amanda Leaders <alsleaders@gmail.com>
2019-06-03 22:03:24 -0400

    it works and is styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 import FibWithClass from './components/FibWithClass'
 
 class App extends Component {
   render() {
     return (
       <>
-        <HelloWorld />
         <FibWithClass />
       </>
     )
--- src/components/FibWithClass.js
+++ src/components/FibWithClass.js
@@ -1,29 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class FibWithClass extends Component {
-  state = {
-    previousNumber: '0',
-    currentNumber: '1',
-    count: '1',
-    sequence: []
+export default function FibWithClass() {
+  const [twoAgo, setTwoAgo] = useState(0)
+  const [lastOne, setLastOne] = useState(0)
+  const [current, setCurrent] = useState(1)
+
+  console.log('did this button work')
+  console.log({ twoAgo })
+  console.log({ lastOne })
+  console.log({ current })
+
+  const addFib = () => {
+    setCurrent(current + lastOne)
+    setLastOne(current)
+    setTwoAgo(lastOne)
   }
 
-  addFib = () => {
-    console.log({ previousNumber })
-    // count.push = currentNumber + previousNumber
-    this.setState({
-      previousNumber: currentNumber,
-      currentNumber: count,
-      count
-    })
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.addFib()}>Next number</button>
-      </div>
-    )
-  }
+  return (
+    <>
+      <h1>Fibonacci Numbers!</h1>
+      <main className="be-centered">
+        <button onClick={addFib}>Next number</button>
+        <p className="display">{current}</p>
+        <button
+          onClick={() => {
+            setTwoAgo(0)
+            setLastOne(0)
+            setCurrent(1)
+          }}
+        >
+          Reset
+        </button>
+      </main>
+    </>
+  )
 }
-
-export default FibWithClass
