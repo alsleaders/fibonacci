31d2445fb2d954b009c15e7d54a200a9015d9f25
Amanda Leaders <alsleaders@gmail.com>
2019-06-03 22:37:49 -0400

    timer works, need to hook to count

--- src/components/FibWithClass.js
+++ src/components/FibWithClass.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function FibWithClass() {
   const [twoAgo, setTwoAgo] = useState(0)
   const [lastOne, setLastOne] = useState(0)
   const [current, setCurrent] = useState(1)
+  const [timer, setTimer] = useState(1)
 
   console.log('did this button work')
   console.log({ twoAgo })
@@ -15,6 +16,12 @@ export default function FibWithClass() {
     setLastOne(current)
     setTwoAgo(lastOne)
   }
+
+  useEffect(() => {
+    setInterval(() => {
+      setTimer(lastSecond => lastSecond + 1)
+    }, 1000)
+  }, '')
 
   return (
     <>
@@ -31,6 +38,7 @@ export default function FibWithClass() {
         >
           Reset
         </button>
+        {/* <h3>{timer}</h3> */}
       </main>
     </>
   )
